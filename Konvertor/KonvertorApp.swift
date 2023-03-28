4f50a322c0087e715d8aacf5143a2d6d4b9539a3
OndraHa <Ondrejhadraba@gmail.com>
2023-03-28 22:32:18 +0200

    Initial Commit

---
+++ Konvertor/KonvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  KonvertorApp.swift
+//  Konvertor
+//
+//  Created by Ondřej Hadraba on 28.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct KonvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
